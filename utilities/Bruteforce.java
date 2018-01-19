62b99308699acc6cedcc758f2fc93bfa2d5027b2
Lunarantic <lunarantic@users.noreply.github.com>
2018-01-18 23:27:55 -0500

    Plan structure of Brute Forcing

---
+++ utilities/Bruteforce.java
@@ -0,0 +1,21 @@
+package utilities;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Bruteforce {
+
+	public List<String> stringsForSizeWith(int size, boolean digits, boolean alphabets, boolean specials) {
+		if(size > 6){
+			return null;
+		}
+		List<String> bruteList = new LinkedList<>();
+		/*
+		 * Generate Strings as per requirements
+		 */
+		return bruteList;
+	}
+	
+	// A method that will call another method with strings for size range and datas required not provide a List of String
+	// will stop when callable method returns true
+}
