1213339ae4c76402dac07c2afbe095818b5c10cc
Sravanthi Kanchi <shravik593@gmail.com>
2018-09-28 15:19:21 -0400

    Updated Commons.java
    Commented on the important checks to be performed on the object. The isEmpty string function is moved to String.java

--- extensions/datatypes/Commons.java
+++ extensions/datatypes/Commons.java
@@ -9,28 +9,33 @@ public class Commons {
 
     public static boolean isEmpty(Object sSource) {
         boolean returnValue = true;
-        if (sSource instanceof String) {
-            returnValue = isEmpty((String) sSource);
+        // if the object is null, it returns false 
+        if (null != sSource) {
+            return false;
+        }    
+        // checks if the object is an instance of String 
+       else if (sSource instanceof String) {
+            returnValue = Strings.isEmpty((String) sSource);
+        //checks if the object is an instance of Collection     
         } else if (sSource instanceof Collection) {
             if (sSource instanceof List<?> && ((List<?>) sSource).size() > 0) {
                 returnValue = false;
+          //checks if the object is an instance of a Set
             } else if (sSource instanceof Set<?> && ((Set<?>) sSource).size() > 0) {
                 returnValue = false;
             }
+          // checks if the object is an instance of a map  
         } else if (sSource instanceof Map) {
             if (((Map<?, ?>) sSource).size() > 0) {
                 returnValue = false;
             }
-        } else if (null != sSource) {
-            return false;
         }
         return returnValue;
     }
 
-    public static boolean isEmpty(String sSource) {
-        if (null == sSource || "".equals(sSource.trim()) || "null".equalsIgnoreCase(sSource.trim())) {
-            return true;
-        }
-        return false;
-    }
+    
 }
+
+
+
+
