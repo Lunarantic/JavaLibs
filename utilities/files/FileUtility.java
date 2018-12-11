ef1c84bc51d166fa6e3c9753b908ed67bc86a8cd
Lunarantic <26676464+Lunarantic@users.noreply.github.com>
2018-12-10 23:10:38 -0500

    added file copying functionality

--- utilities/files/FileUtility.java
+++ utilities/files/FileUtility.java
@@ -1,79 +1,92 @@
 package utilities.files;
 
-import java.io.BufferedWriter;
-import java.io.Closeable;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class FileUtility {
 
 	public static boolean deleteFile(String fileName) {
-		File file = new File(fileName);
-		return file.delete();
+		return (new File(fileName)).delete();
 	}
 
-	public static String readFile(File file) {
-		try {
-			return readFile(new FileInputStream(file));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return "";
-	}
-
-	public static String readFile(InputStream inputStream) {
-
+	public static String readFile(InputStream inputStream) throws IOException {
 		StringBuilder stringBuilder = new StringBuilder();
 		int ch;
 		try {
 			while ((ch = inputStream.read()) != -1) {
 				stringBuilder.append((char) ch);
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
 		} finally {
 			close(inputStream);
 		}
 		return stringBuilder.toString();
 	}
 
-	public static String readFile(String filePath) {
-		File file = new File(filePath);
-		return readFile(file);
+    public static String readFile(File file) throws IOException {
+        return readFile(new FileInputStream(file));
+    }
+
+	public static String readFile(String fileName) throws IOException {
+	    return readFile(new FileInputStream(fileName));
 	}
 
-	public static void writeTotFile(String fileName, String content) {
-		FileWriter fileWriter = null;
-		BufferedWriter bufferedWriter = null;
-		try {
-			File file = new File(fileName);
-			if (!file.exists()) {
-                file.createNewFile();
+    public static void writeToFile(FileWriter fileWriter, String content) throws IOException {
+        BufferedWriter bufferedWriter = null;
+	    try {
+            bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(content);
+            bufferedWriter.flush();
+        } finally {
+            close(bufferedWriter);
+            close(fileWriter);
+        }
+    }
+
+    public static void writeToFile(File file, String content) throws IOException {
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        writeToFile(new FileWriter(file, true), content);
+    }
+
+    public static void writeToFile(String fileName, String content) throws IOException {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        writeToFile(new FileWriter(fileName, true), content);
+    }
+
+    public static void copyFile(FileInputStream fileInputStream, FileOutputStream fileOutputStream) throws IOException {
+        try {
+            byte[] buf = new byte[1024];
+            int i;
+            while ((i = fileInputStream.read(buf)) != -1)
+                fileOutputStream.write(buf, 0 , i);
+        } finally {
+            close(fileInputStream);
+            close(fileOutputStream);
+        }
+    }
+
+    public static void copyFile(File input, File output) throws IOException {
+        if (!input.exists())
+            return;
+        if (!output.exists())
+            output.createNewFile();
+        copyFile(new FileInputStream(input), new FileOutputStream(output));
+    }
+
+    public static void copyFile(String inputFileName, String outputFileName) throws IOException {
+	    copyFile(new FileInputStream(inputFileName), new FileOutputStream(outputFileName));
+    }
+
+    public static void close(Closeable closeable) {
+        if (null != closeable) {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-			fileWriter = new FileWriter(fileName, true);
-			bufferedWriter = new BufferedWriter(fileWriter);
-			bufferedWriter.write(content);
-			bufferedWriter.flush();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			close(bufferedWriter);
-			close(fileWriter);
-		}
-	}
-	
-	public static void close(Closeable closeable) {
-		if (null != closeable) {
-			try {
-				closeable.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        }
+    }
 }
